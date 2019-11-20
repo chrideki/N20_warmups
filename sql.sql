237365e40fa421b4c253bd542ec37777660ba6af
Christian Goldoni <christian.goldoni@gmail.com>
2019-11-20 10:47:13 -0500

    added shorter version

--- sql.sql
+++ sql.sql
@@ -37,3 +37,28 @@ GROUP BY customer_id, ship_country
                 ELSE ROUND(cast(difference as numeric), 2)
             END AS rounded_difference
         FROM data_with_difference;
+
+--shorter version
+
+WITH customer_total_spending AS(
+SELECT
+    customer_id,
+    ship_country,
+    SUM(unit_price * quantity) AS total_spending
+FROM orders
+JOIN order_details USING(order_id)
+GROUP BY customer_id, ship_country
+), ranked_total_spending AS(
+    SELECT
+        *,
+        RANK() OVER(PARTITION BY ship_country ORDER BY total_spending DESC) AS rank,
+        LEAD(total_spending, 1) OVER(PARTITION BY ship_country ORDER BY ship_country) as next_highest
+    FROM customer_total_spending
+    )
+    SELECT
+        *,
+        CASE
+            WHEN next_highest IS NULL THEN 0
+            ELSE ROUND(cast(total_spending - next_highest as numeric), 2)
+        END AS rounded_difference
+    FROM ranked_total_spending;
